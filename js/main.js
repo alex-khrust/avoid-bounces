e3e65beb5fa8247b0ec6272d6629da51e991c0ef
Александр Хрусталёв <st.alex.ser@gmail.com>
2019-01-30 19:24:54 +0200

    - добавил анимацию прокрутки к пункту меню  - убрал возможность закрывать активный айтем аккордиона

--- js/main.js
+++ js/main.js
@@ -11,8 +11,47 @@ $(document).ready(function ()  {
 //       $('.hamburger-menu').toggleClass('down');
 //     })
 //   })();
+// -----------------------------------------------------------------------
+// Скрипт для присвоения пункту меню класса актив ------------------------------
   
-  // Добавление класса active пунктам меню --------------------------------
+  $(document).on("scroll", onScroll);
+  
+  $('a[href^="#"]').on('click', function (e) {
+    e.preventDefault();
+    $(document).off("scroll");
+    
+    $('a').each(function () {
+      $(this).removeClass('active');
+    });
+    $(this).addClass('active');
+    
+    var target = this.hash;
+    $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top + 2
+    }, 500, 'swing', function () {
+      window.location.hash = target;
+      $(document).on("scroll", onScroll);
+    });
+  });
+  
+  
+  function onScroll(event) {
+    var scrollPosition = $(document).scrollTop();
+    $('nav a').each(function () {
+      var currentLink = $(this);
+      var refElement = $(currentLink.attr("href"));
+      if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+        $('nav ul li a').removeClass("active");
+        currentLink.addClass("active");
+      }
+      else {
+        currentLink.removeClass("active");
+      }
+    });
+  }
+//-----------------------------------------------------------------------
+// Добавление класса active пунктам меню --------------------------------
   // $('.type li a').click(function(){
   //   $(".type li a").removeClass('active');
   //   $(this).addClass('active');
@@ -72,8 +111,8 @@ $(document).ready(function ()  {
     
     items.on("click",function(){
       if($(this).hasClass("active")) {
-        $(this).removeClass("active");
-        $(this).next().removeClass("open");
+        // $(this).removeClass("active");
+        // $(this).next().removeClass("open");
       } else {
         $(this).siblings().removeClass("active");
         $(this).next().siblings().removeClass("open");
